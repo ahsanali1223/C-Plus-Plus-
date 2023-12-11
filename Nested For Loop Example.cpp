ea1f70686e13bc69ad2c2fc07a2d924ee277b7a7
ahsanali1223 <111628707+ahsanali1223@users.noreply.github.com>
2023-12-11 17:23:49 +0500

    Add files via upload

---
+++ Nested For Loop Example.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main() 
+{
+    for (int i=0;i<5;i++)
+    {
+
+        for (int j = 0; j < 5; j++)
+        {
+            if(j<4-i)
+               cout<<" ";
+            else
+               cout<<"*";
+        }
+        cout<<endl;
+    }
+    return 0;
+}
